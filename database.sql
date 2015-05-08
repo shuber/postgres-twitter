142c1eb776a9a883555ac1407c184211825db723
Sean Huber <github@shuber.io>
2015-05-08 15:50:28 -0700

    Organize things a bit, begin implenting a trigger

--- database.sql
+++ database.sql
@@ -1,16 +1,24 @@
--- Notes/ideas
---
--- * Consider dropping tags.id and making tags.name the primary key.
---   Then taggings.tag_id can be replaced with taggings.name.
--- * Make tables "private". Create views for application to interact with.
---   This should make "migrations" easier as well since applications don't
---   interact with the tables directly.
--- * Use default schemas for different parts of the application since they
---   can be replicated with different rules. Maybe there can even be some
---   kind of "cache" schema.
+-- ############################################################################
+-- # Notes and ideas
+-- ############################################################################
+
+-- Consider dropping tags.id and making tags.name the primary key.
+-- Then taggings.tag_id can be replaced with taggings.name.
+
+-- Make tables "private". Create views for application to interact with.
+-- This should make "migrations" easier as well since applications don't
+-- interact with the tables directly.
+
+-- Use default schemas for different parts of the application since they
+-- can be replicated with different rules. Maybe there can even be some
+-- kind of "cache" schema.
 
 
--- Drop everything (in reverse)
+-- ############################################################################
+-- # Drop everything in reverse (for development)                        DANGER
+-- ############################################################################
+DROP TRIGGER IF EXISTS parse_hashtags ON tweets;
+DROP FUNCTION IF EXISTS parse_hashtags_from_post();
 DROP TABLE IF EXISTS "taggings";
 DROP TABLE IF EXISTS "tags";
 DROP TABLE IF EXISTS "tweets";
@@ -18,44 +26,90 @@ DROP EXTENSION IF EXISTS "uuid-ossp";
 DROP SCHEMA IF EXISTS "public";
 
 
--- Schemas
+-- ############################################################################
+-- # Schemas
+-- ############################################################################
 CREATE SCHEMA IF NOT EXISTS "public";
 
 
--- Extensions
+-- ############################################################################
+-- # Extensions
+-- ############################################################################
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 
+-- ############################################################################
+-- # Tables
+-- ############################################################################
+
 -- Tweets
 CREATE TABLE IF NOT EXISTS tweets (
-  id       uuid PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
-  post     varchar(140) NOT NULL,
-  created  timestamp WITH TIME ZONE NOT NULL DEFAULT current_timestamp,
-  updated  timestamp WITH TIME ZONE NOT NULL DEFAULT current_timestamp
+  id        uuid PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
+  post      text NOT NULL,
+  hashtags  text[] NOT NULL DEFAULT '{}',
+  created   timestamp WITH TIME ZONE NOT NULL DEFAULT current_timestamp,
+  updated   timestamp WITH TIME ZONE NOT NULL DEFAULT current_timestamp
 );
+
+ALTER TABLE tweets ADD CONSTRAINT post_length CHECK (char_length(post) <= 140);
+
 
 
 -- Tags
 CREATE TABLE IF NOT EXISTS tags (
   id       uuid PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
-  name     varchar(140) NOT NULL UNIQUE,
-  tweets   integer NOT NULL DEFAULT 0 CHECK (tweets >= 0),
+  name     text NOT NULL UNIQUE,
+  tweets   integer NOT NULL DEFAULT 0,
   created  timestamp WITH TIME ZONE NOT NULL DEFAULT current_timestamp,
   updated  timestamp WITH TIME ZONE NOT NULL DEFAULT current_timestamp
 );
 
-CREATE UNIQUE INDEX ON tags (lower(name));
+ALTER TABLE tags ADD CONSTRAINT tweets_count CHECK (tweets >= 0);
 
 
 -- Taggings
 CREATE TABLE IF NOT EXISTS taggings (
-  tag_id    uuid NOT NULL REFERENCES tags (id) ON UPDATE CASCADE ON DELETE CASCADE,
-  tweet_id  uuid NOT NULL REFERENCES tweets (id) ON UPDATE CASCADE ON DELETE CASCADE,
+  tag_id    uuid NOT NULL,
+  tweet_id  uuid NOT NULL,
   PRIMARY KEY(tag_id, tweet_id)
 );
 
+ALTER TABLE taggings
+  ADD CONSTRAINT tag_fk FOREIGN KEY (tag_id) REFERENCES tags (id)
+  MATCH FULL ON UPDATE CASCADE ON DELETE CASCADE;
 
--- Seed data
+ALTER TABLE taggings
+  ADD CONSTRAINT tweet_fk FOREIGN KEY (tweet_id) REFERENCES tweets (id)
+  MATCH FULL ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+-- ############################################################################
+-- # Functions
+-- ############################################################################
+CREATE FUNCTION parse_hashtags_from_post()
+  RETURNS trigger AS $$
+    BEGIN
+      -- Parse tweets.post with something like the following regex below and
+      -- update tweets.hashtag.
+      --
+      --   regexp_matches(NEW.post, '#(\S+)', 'g');
+
+      RETURN NULL;
+    END;
+  $$ LANGUAGE plpgsql;
+
+
+-- ############################################################################
+-- # Triggers
+-- ############################################################################
+CREATE TRIGGER parse_hashtags
+  AFTER INSERT ON tweets
+  FOR EACH ROW EXECUTE PROCEDURE parse_hashtags_from_post();
+
+
+-- ############################################################################
+-- # Seed data
+-- ############################################################################
 INSERT INTO tweets (post) VALUES
   ('My first tweet! #hello-world'),
   ('My second tweet! #hello-world #hello-world-again'),
@@ -63,7 +117,9 @@ INSERT INTO tweets (post) VALUES
   ('I am! #imhungry #metoo #gimmefood #now');
 
 
--- Debug output
+-- ############################################################################
+-- # Debug output
+-- ############################################################################
 SELECT * FROM tweets;
 SELECT * FROM taggings;
 SELECT * FROM tags;
